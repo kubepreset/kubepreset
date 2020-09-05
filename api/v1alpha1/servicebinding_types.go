a33f11c1e07044edecf6a806f5e216d35666581f
Baiju Muthukadan <baiju.m.mail@gmail.com>
2020-09-05 08:13:41 +0530

    Add minimal API
    Signed-off-by: Baiju Muthukadan <baiju.m.mail@gmail.com>

--- api/v1alpha1/servicebinding_types.go
+++ api/v1alpha1/servicebinding_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,14 +29,101 @@ type ServiceBindingSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ServiceBinding. Edit ServiceBinding_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Application resource to inject the binding info.
+	// It could be any process running within a container.
+	// From the spec:
+	// A Service Binding resource **MUST** define a `.spec.application`
+	// which is an `ObjectReference`-like declaration to a `PodSpec`-able
+	// resource.  A `ServiceBinding` **MAY** define the application
+	// reference by-name or by-[label selector][ls]. A name and selector
+	// **MUST NOT** be defined in the same reference.
+	Application *Application `json:"application,omitempty"`
+
+	// Service referencing the binding secret
+	// From the spec:
+	// A Service Binding resource **MUST** define a `.spec.service` which is
+	// an `ObjectReference`-like declaration to a Provisioned Service-able
+	// resource.
+	Service *Reference `json:"service,omitempty"`
+}
+
+type Reference struct {
+	// API version of the referent.
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+
+	// Kind of the referent.
+	// +optional
+	Kind string `json:"kind,omitempty"`
+
+	// Name of the referent.
+	// Mutually exclusive with Selector.
+	// +optional
+	Name string `json:"name,omitempty"`
+}
+
+type Application struct {
+	Reference `json:",inline"`
+
+	// Selector of the referents.
+	// Mutually exclusive with Name.
+	// +optional
+	Selector *metav1.LabelSelector `json:"selector,omitempty"`
 }
 
 // ServiceBindingStatus defines the observed state of ServiceBinding
 type ServiceBindingStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+}
+
+type Status struct {
+	// ObservedGeneration is the 'Generation' of the Service that
+	// was last processed by the controller.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Conditions the latest available observations of a resource's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// ConditionReady specifies that the resource is ready.
+// For long-running resources.
+const ConditionReady ConditionType = "Ready"
+
+type Conditions []Condition
+
+type ConditionType string
+
+type VolatileTime struct {
+	Inner metav1.Time `json:",inline"`
+}
+
+type Condition struct {
+	// Type of condition.
+	// +required
+	Type ConditionType `json:"type" description:"type of status condition"`
+
+	// Status of the condition, one of True, False, Unknown.
+	// +required
+	Status corev1.ConditionStatus `json:"status" description:"status of the condition, one of True, False, Unknown"`
+
+	// LastTransitionTime is the last time the condition transitioned from one status to another.
+	// We use VolatileTime in place of metav1.Time to exclude this from creating equality.Semantic
+	// differences (all other things held constant).
+	// +optional
+	LastTransitionTime VolatileTime `json:"lastTransitionTime,omitempty" description:"last time the condition transit from one status to another"`
+
+	// The reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty" description:"one-word CamelCase reason for the condition's last transition"`
+
+	// A human readable message indicating details about the transition.
+	// +optional
+	Message string `json:"message,omitempty" description:"human-readable message indicating details about last transition"`
 }
 
 // +kubebuilder:object:root=true
