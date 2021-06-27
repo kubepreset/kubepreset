d6ba4d746fadff0ef605427e7ccd4e1f8dbb0606
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-06-27 13:52:31 +0530

    Set logger
    Signed-off-by: Baiju Muthukadan <baiju.m.mail@gmail.com>

--- main.go
+++ main.go
@@ -81,6 +81,7 @@ func main() {
 	if err = (&bindingcontrollers.ServiceBindingReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
+		Log:    ctrl.Log.WithName("bindingcontrollers.servicebinding").WithName("ServiceBinding"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "ServiceBinding")
 		os.Exit(1)
